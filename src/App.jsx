1f047f66afa4a0e1d0ac00b27b1ba8d7f51b7d22
Angel-Acer <licangelgodinez@hotmail.com>
2023-09-23 23:18:21 -0500

    flipdown clock added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
 function App() {
+
+
   return (
     // <div className="content">{import.meta.env.VITE_NAME}
     <div className="content">
-      <span className="goldenTitle">Barbie<br />Party</span>
+      <span className="goldenTitle">Barbie Party
+        <br />
+        <FlipClockCountdown
+          to={import.meta.env.VITE_DATE}
+          labels={['Días', 'Horas', 'Minutos', 'Segundos']}
+          labelStyle={{ fontSize: '.3em', fontWeight: 500, textTransform: 'uppercase', color: 'black' }}
+          digitBlockStyle={{ width: '1em', height: '1.5em', fontSize: '.3em', color: 'white', background: `#${import.meta.env.VITE_PINK_COLOR}` }}
+          dividerStyle={{ color: 'white', height: 1 }}
+          separatorStyle={{ color: `#${import.meta.env.VITE_PINK_COLOR}`, size: '.1em' }}
+          duration={0.5}
+          
+        />
+
+      </span>
     
     </div>
   )
